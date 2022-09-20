1dda928357de085c52e87bbd5238cc54b941cd5f
simorico <oli.simorico@gmail.com>
2022-09-20 15:16:13 +0200

    init charte

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+    colors: {
+      'primary': '#1f71a5',
+      'secondary': '#275e9d',
+      'tpkwhite': '#f7f7f7',
+      'tpkerror': '#e44e4d',
+      'tpkvalidate': '#50e2ad',
+      'grey1': '#e1e1e0',
+      'grey2': '#a1a1a0',
+      'grey3': '#71716f',
+      'grey4': '"3c3c3b',
+    }
+   },
   },
   plugins: [],
 }
