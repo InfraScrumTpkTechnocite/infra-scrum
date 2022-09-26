2374b70313122d443b25032f2b9d285e205344a8
AndyTierce <andytierce@gmail.com>
2022-09-26 14:57:50 +0200

    creation admin module avec users overview

--- packages/frontend/src/app/app-routing.module.ts
+++ packages/frontend/src/app/app-routing.module.ts
@@ -6,6 +6,11 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 
 const routes: Routes = [
     { path: '', redirectTo: 'index', pathMatch: 'full' },
+    {
+        path: 'admin',
+        loadChildren: () =>
+            import('./admin/admin.module').then((m) => m.AdminModule)
+    },
     { path: 'index', component: IndexComponent },
     { path: 'login', component: LoginComponent },
     { path: '**', component: PageNotFoundComponent }
