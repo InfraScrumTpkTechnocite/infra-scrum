f0b11652f969717e5b00a0e51a1fe9195821e289
AndyTierce <andytierce@gmail.com>
2022-09-27 15:00:01 +0200

    ajout bouton logout dans header quand on se login

--- packages/frontend/src/app/form/login/login.component.ts
+++ packages/frontend/src/app/form/login/login.component.ts
@@ -3,42 +3,52 @@ import { AuthService } from 'src/app/services/auth.service';
 import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  profileForm = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
-  });
+    profileForm = new FormGroup({
+        username: new FormControl(''),
+        password: new FormControl('')
+    });
 
-  constructor(private authService: AuthService) { }
+    constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {}
+    ngOnInit(): void {}
 
-  login() {
-    console.log(`username : ${this.profileForm.controls.username.value}`);
-    console.log(`password : ${this.profileForm.controls.password.value}`);
+    login() {
+        console.log(`username : ${this.profileForm.controls.username.value}`);
+        console.log(`password : ${this.profileForm.controls.password.value}`);
 
-//    this.authService.login('gilles', 'gilles').subscribe(data => console.log('authlogin OK'));
-    if(this.profileForm.controls.username.value && this.profileForm.controls.password.value)
-    this.authService.login(
-      this.profileForm.controls.username.value,
-      this.profileForm.controls.password.value
-      ).subscribe(data => console.log('authlogin OK'));
-  }
+        //    this.authService.login('gilles', 'gilles').subscribe(data => console.log('authlogin OK'));
+        if (
+            this.profileForm.controls.username.value &&
+            this.profileForm.controls.password.value
+        )
+            this.authService
+                .login(
+                    this.profileForm.controls.username.value,
+                    this.profileForm.controls.password.value
+                )
+                .subscribe((data) => console.log('authlogin OK'));
+    }
 
-  onSubmit() {
-    console.log(`username : ${this.profileForm.controls.username.value}`);
-    console.log(`password : ${this.profileForm.controls.password.value}`);
+    onSubmit() {
+        console.log(`username : ${this.profileForm.controls.username.value}`);
+        console.log(`password : ${this.profileForm.controls.password.value}`);
 
-//    this.authService.login('gilles', 'gilles').subscribe(data => console.log('authlogin OK'));
-    if(this.profileForm.controls.username.value && this.profileForm.controls.password.value)
-    this.authService.login(
-      this.profileForm.controls.username.value,
-      this.profileForm.controls.password.value
-      ).subscribe(data => console.log('authlogin OK'));
-
-  }
+        //    this.authService.login('gilles', 'gilles').subscribe(data => console.log('authlogin OK'));
+        if (
+            this.profileForm.controls.username.value &&
+            this.profileForm.controls.password.value
+        )
+            this.authService
+                .login(
+                    this.profileForm.controls.username.value,
+                    this.profileForm.controls.password.value
+                )
+                .subscribe((data) => console.log('authlogin OK'));
+        this.authService.isLoggedIn = true;
+    }
 }
--- packages/frontend/src/app/services/auth.service.ts
+++ packages/frontend/src/app/services/auth.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
+    constructor(private http: HttpClient) {}
 
-  constructor( private http: HttpClient ) { }
+    isLoggedIn: boolean = false;
 
-  login(username: string, password: string) {
-    return this.http.post<any>('/backend/auth/login', {username, password})
-      .pipe(map( (token) => {
-        localStorage.setItem('jwt-token', token.access_token);
-        return token;
-      }))
-  }
+    login(username: string, password: string) {
+        return this.http
+            .post<any>('/backend/auth/login', { username, password })
+            .pipe(
+                map((token) => {
+                    localStorage.setItem('jwt-token', token.access_token);
+                    return token;
+                })
+            );
+    }
 }
