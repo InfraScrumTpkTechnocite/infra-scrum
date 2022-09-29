737ba8b148d095e1a442fb0d2bee958bd6dbb195
bendeg <benoitdeg@gmail.com>
2022-09-29 09:05:38 +0200

    users service (header, token)

--- packages/frontend/src/app/services/user.service.ts
+++ packages/frontend/src/app/services/user.service.ts
@@ -10,11 +10,11 @@ import { Observable, of, tap } from 'rxjs';
 export class UserService {
 
   //bearerToken à changer
-  bearerToken :string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImdpbGxlcyIsInN1YiI6IjYzNWUwMDBhLTUyOTEtNGMwOS05MmViLTQxZWU2OTRkZWI3MiIsImlhdCI6MTY2NDI2MzUwMSwiZXhwIjoxNjY5NDQ3NTAxfQ.ChkBQt-6jZ3xLBPexdSIjwU--PaUJ0qoLQMXVuaqvNw";
+  //bearerToken :string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImdpbGxlcyIsInN1YiI6IjYzNWUwMDBhLTUyOTEtNGMwOS05MmViLTQxZWU2OTRkZWI3MiIsImlhdCI6MTY2NDI2MzUwMSwiZXhwIjoxNjY5NDQ3NTAxfQ.ChkBQt-6jZ3xLBPexdSIjwU--PaUJ0qoLQMXVuaqvNw";
   httpOptions = {
     headers : new HttpHeaders({
       'Content-Type': 'application/json',
-      Authorization : 'Bearer ' + this.bearerToken
+      Authorization : 'Bearer ' + localStorage.getItem('jwt-token')
     })
   };
 
@@ -30,7 +30,8 @@ export class UserService {
   }
 
   createUser(user: User): Observable<User> {
-
+    user.role = "f5e44de5-4cfb-42af-8609-0d8d9867f517";
+    
     return this.httpClient.post<any>("/backend/users", JSON.stringify(user), this.httpOptions)
     .pipe(
         tap((response) => this.log(response)),
