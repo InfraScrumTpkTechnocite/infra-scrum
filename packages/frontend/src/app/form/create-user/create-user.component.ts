7ff13c36a80d6069e6526fa52da886f1b9c07b8a
AndyTierce <andytierce@gmail.com>
2022-09-30 16:22:51 +0200

    Affichage dynamique titre component dans le header

--- packages/frontend/src/app/form/create-user/create-user.component.ts
+++ packages/frontend/src/app/form/create-user/create-user.component.ts
@@ -1,31 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user.model';
+import { HeaderTitleService } from 'src/app/services/header-title.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
-  selector: 'app-create-user',
-  templateUrl: './create-user.component.html',
-  styleUrls: ['./create-user.component.css']
+    selector: 'app-create-user',
+    templateUrl: './create-user.component.html',
+    styleUrls: ['./create-user.component.css']
 })
+export class CreateUserComponent implements OnInit {
+    user: User = new User();
+    @Input() verifPassword: string;
 
-export class CreateUserComponent{
-  
-  user: User = new User();
-  @Input() verifPassword: string;
-  
-  constructor(
-    private userService: UserService,
-    private router: Router,
-  ) {
-    this.verifPassword = ""
-  }
+    constructor(
+        private userService: UserService,
+        private router: Router,
+        private headerTitleService: HeaderTitleService
+    ) {
+        this.verifPassword = '';
+    }
+    ngOnInit(): void {
+        this.headerTitleService.setTitle('InfraScrum');
+    }
 
-  onSubmit() {
-    this.userService.createUser(this.user).subscribe(
-      (user: User) => {
-        if(user) this.router.navigate(['/login'])
-      });
-  }
+    onSubmit() {
+        this.userService.createUser(this.user).subscribe((user: User) => {
+            if (user) this.router.navigate(['/login']);
+        });
+    }
 }
-
