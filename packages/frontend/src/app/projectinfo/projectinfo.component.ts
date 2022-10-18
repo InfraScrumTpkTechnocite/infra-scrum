181f4d249a6735fcbce605f400240179ae07f568
AndyTierce <andytierce@gmail.com>
2022-10-18 16:39:54 +0200

    simple edit project in projects dashboard

--- packages/frontend/src/app/projectinfo/projectinfo.component.ts
+++ packages/frontend/src/app/projectinfo/projectinfo.component.ts
@@ -1,28 +1,116 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { HotToastService } from '@ngneat/hot-toast';
+import { Project } from '../models/project.model';
+import { User } from '../models/user.model';
 import { UserProject } from '../models/userproject.model';
+import { HeaderTitleService } from '../services/header-title.service';
+import { ProjectService } from '../services/project.service';
+import { UserService } from '../services/user.service';
+import { UserprojectService } from '../services/userproject.service';
 
 @Component({
-  selector: 'app-projectinfo',
-  templateUrl: './projectinfo.component.html',
-  styleUrls: ['./projectinfo.component.css']
+    selector: 'app-projectinfo',
+    templateUrl: './projectinfo.component.html',
+    styleUrls: ['./projectinfo.component.css']
 })
 export class ProjectinfoComponent {
-  @Input() userProject!: UserProject;
-  isEditNewProject: boolean = false;
-  selectedPictureFile?: File;
+    @Input() userProject!: UserProject;
+    isEditNewProject: boolean = false;
+    // selectedPictureFile?: File;
+    showErrorMessage: boolean = false;
+    errorMessage: string = '';
+    userProjects: any;
 
-  constructor() { }
+    constructor(
+        private headerTitleService: HeaderTitleService,
+        private userProjectService: UserprojectService,
+        private userService: UserService,
+        private projectService: ProjectService,
+        private toastService: HotToastService
+    ) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        const userProjectsObserver = {
+            next: (value: any) => {
+                this.userProjects = value; //tous les user-projects (contenants toutes les infos du projet)
+                this.headerTitleService.setUserProjects(value);
+                var usertmp: any = localStorage.getItem('user');
+                var user: User = JSON.parse(usertmp);
+                if (user) this.headerTitleService.setUsername(user);
+            },
+            error: (err: Error) => {
+                console.log(`${err}`);
+            },
+            complete: () => {
+                console.log(`get user's projects completed.`);
+            }
+        };
 
-  editProjectModel() {
-    this.isEditNewProject = !this.isEditNewProject;
-    console.log(`project name = ${this.userProject.project.name}, projet id = ${this.userProject.project.id}`)
-  }
+        const userObserver = {
+            next: (response: User) => {
+                localStorage.setItem('user', JSON.stringify(response));
 
-  onFileSelected(event: any) {
-    this.selectedPictureFile = event.target.files[0];
-    console.log(`Selected picture file : ${this.selectedPictureFile?.name}`);
-  }
+                if (response.id)
+                    this.userProjectService
+                        .findCurrentUserProjects(response.id)
+                        .subscribe(userProjectsObserver);
+            },
+            error: (err: Error) => {
+                console.log(`Error: ${err}`);
+            },
+            complete: () => {
+                console.log(`login.component.ts - get user completed.`);
+            }
+        };
+
+        var username = localStorage.getItem('username');
+        if (username)
+            this.userService
+                .findUserByUsername(username)
+                .subscribe(userObserver);
+    }
+
+    openEditionProject() {
+        this.isEditNewProject = true;
+    }
+
+    updateProjectModel() {
+        console.log(
+            `project name = ${this.userProject.project.name}, projet id = ${this.userProject.project.id}`
+        );
+
+        const projectObserver = {
+            next: (project: Project) => {
+                console.log('project edition');
+            },
+            error: (err: any) => {
+                this.showErrorMessage = true;
+                this.errorMessage = err.error.driverError.detail;
+                console.log(
+                    `Erreur édition projet : ${err.error['driverError'].detail}`
+                );
+                this.toastService.error(
+                    `Error during project edition <br><br>${err.error.driverError.detail}`
+                );
+            },
+            complete: () => {
+                console.log(`projects.component.ts - edit project completed.`);
+                this.toastService.success('Project edited !');
+            }
+        };
+        // console.warn(this.userProject);
+
+        this.projectService
+            .update(this.userProject.project)
+            .subscribe(projectObserver);
+
+        this.isEditNewProject = false;
+    }
+
+    // onFileSelected(event: any) {
+    //     this.selectedPictureFile = event.target.files[0];
+    //     console.log(
+    //         `Selected picture file : ${this.selectedPictureFile?.name}`
+    //     );
+    // }
 }
--- packages/frontend/src/app/services/project.service.ts
+++ packages/frontend/src/app/services/project.service.ts
@@ -4,37 +4,50 @@ import { catchError, Observable, of, tap } from 'rxjs';
 import { Project } from '../models/project.model';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ProjectService {
-  
-  httpOptions = {
-    headers : new HttpHeaders({
-      'Content-Type': 'application/json',
-      Authorization : 'Bearer ' + localStorage.getItem('jwt-token')
-    })
-  };
+    httpOptions = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + localStorage.getItem('jwt-token')
+        })
+    };
 
-  constructor(
-    private httpClient : HttpClient
-  ) { }
+    constructor(private httpClient: HttpClient) {}
 
-  private handleError(error: Error, errorValue: any){
-    console.error(error);
-    return of(errorValue);
-  }
+    private handleError(error: Error, errorValue: any) {
+        console.error(error);
+        return of(errorValue);
+    }
 
-  private log(response: any){
-    console.table(response);
-  }
+    private log(response: any) {
+        console.table(response);
+    }
 
-  create(project: Project): Observable<Project> {
+    create(project: Project): Observable<Project> {
+        return this.httpClient
+            .post<any>(
+                '/backend/projects',
+                JSON.stringify(project),
+                this.httpOptions
+            )
+            .pipe(
+                tap((response) => this.log(response)),
+                catchError((error) => this.handleError(error, null))
+            );
+    }
 
-    return this.httpClient.post<any>("/backend/projects", JSON.stringify(project), this.httpOptions)
-    .pipe(
-      tap((response) => this.log(response)),
-      catchError((error) => this.handleError(error,null))
-    );
-  }
-
+    update(project: Project): Observable<Project> {
+        return this.httpClient
+            .put<Project>(
+                '/backend/projects/' + project.id,
+                JSON.stringify(project),
+                this.httpOptions
+            )
+            .pipe(
+                tap((response) => this.log(response)),
+                catchError((error) => this.handleError(error, null))
+            );
+    }
 }
