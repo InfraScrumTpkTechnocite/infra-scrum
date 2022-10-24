79f1ab7163db7efbeef87fca9bc88053236b6b3b
simorico <oli.simorico@gmail.com>
2022-10-24 13:35:37 +0200

    Fonctionnalité EditTaskq

--- packages/frontend/src/app/form/edit-new-tasks/edit-new-tasks.component.ts
+++ packages/frontend/src/app/form/edit-new-tasks/edit-new-tasks.component.ts
@@ -1,8 +1,19 @@
 import { Component} from '@angular/core';
+import { FormControl, NgForm } from '@angular/forms';
+import { Task } from 'src/app/models/task.model';
 
 @Component({
   selector: 'app-edit-new-tasks',
   templateUrl: './edit-new-tasks.component.html',
   styleUrls: ['./edit-new-tasks.component.css']
 })
-export class EditNewTasksComponent {}
+export class EditNewTasksComponent {
+  task: Task = new Task(); 
+
+  onSubmit() {
+    // console.log(formEditTask.value);
+    // console.log(formEditTask.valid);
+    
+    
+  }
+}
