cff017cf123f2d9e5f41e38dc736c05baa86bc49
simorico <oli.simorico@gmail.com>
2022-10-25 10:17:39 +0200

    on init fonction

--- packages/frontend/src/app/form/edit-new-tasks/edit-new-tasks.component.ts
+++ packages/frontend/src/app/form/edit-new-tasks/edit-new-tasks.component.ts
@@ -1,17 +1,29 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { FormControl, NgForm } from '@angular/forms';
 import { Task } from 'src/app/models/task.model';
+import { TaskService } from 'src/app/services/task.service';
+import { TasktypeService } from 'src/app/services/tasktype.service';
+import { TaskassignmentService } from 'src/app/services/taskassignment.service';
 
 @Component({
   selector: 'app-edit-new-tasks',
   templateUrl: './edit-new-tasks.component.html',
   styleUrls: ['./edit-new-tasks.component.css']
 })
-export class EditNewTasksComponent {
+export class EditNewTasksComponent implements OnInit {
   task: Task = new Task(); 
 
+  constructor(
+    private taskService: TaskService, 
+    private tasktypeService: TasktypeService,
+    private taskassignementService: TaskassignmentService 
+  ){}
+
+    ngOnInit(): void {
+      
+    }
   onSubmit() {
-    // console.log(formEditTask.value);
+    console.log("hello");
     // console.log(formEditTask.valid);
     
     
