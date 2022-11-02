12e56bc8226d320c2630b1831f4cfbe86b96762f
szmulikowski Vincent <89385456+Vincent1307@users.noreply.github.com>
2022-11-02 21:46:22 +0100

    mofif

--- packages/backend/src/users/users.controller.ts
+++ packages/backend/src/users/users.controller.ts
@@ -61,49 +61,6 @@ export class UsersController {
 
   @Get('/username/:username')
   async findOneByUsername(@Param('username') username: string): Promise<User> {
-    return await this.userService.findOneByUsername(username);
+    return await this.userService.findOneByUsername(username);}
   }
 
-async uploadFile(
-  @UploadedFile(
-      new ParseFilePipe({
-        validators: [
-          new MaxFileSizeValidator({ maxSize: 50000 }),
-          new FileTypeValidator({
-            fileType: new RegExp('(.jpeg|.JPEG|.gif|.GIF|.png|.PNG)$'),
-          }),
-        ],
-      }),
-    )
-    file: Express.Multer.File,
-    @Param('id') id: string,
-  ) {
-    
-    // console.log(file);
-    // console.log(`project id : ${id}`);
-
-    let user: User = new User();
-  
-this.userService.findOne(id).then((result) => {
-  user = result;
-});
-
-readFile(file.path, (err, data) => {
-  if (err) throw err;
-
-  //console.log(data.toString('base64'));
-
-  user.picture = data.toString('base64');
-      this.userService.update(id, user);
-
-      unlink(file.path, (error) => {
-        console.log(error);
-      });
-    });
-
-    return new Observable((subscriber) => {
-      subscriber.next(file);
-      subscriber.complete();
-    });
-  }
-}
