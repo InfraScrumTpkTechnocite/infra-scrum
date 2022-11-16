f940e430ce6d64d31ff93c82bbf20473dbec3023
AndyTierce <andytierce@gmail.com>
2022-11-16 16:17:06 +0100

    ongoing tasks history

--- packages/frontend/src/app/project/components/tasks-history/tasks-history.component.ts
+++ packages/frontend/src/app/project/components/tasks-history/tasks-history.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { KanbanList } from 'src/app/models/kanbanlist.model';
 
 @Component({
     selector: 'app-tasks-history',
@@ -7,14 +8,22 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TasksHistoryComponent implements OnInit {
     isTaskReduced: boolean = true;
+    tasksLists: any = [];
+    projectTasks = [];
 
-    // @Input() kanbanList: any
+    @Input() kanbanList!: KanbanList[];
 
     constructor() {}
 
-    ngOnInit(): void {}
-
-    getTaskOverview() {
-        this.isTaskReduced = !this.isTaskReduced;
+    ngOnInit(): void {
+        this.kanbanList.forEach((knbn) => {
+            this.tasksLists = this.tasksLists.concat(knbn.tasks);
+        });
+        this.tasksLists.sort(function (task1: any, task2: any) {
+            return (
+                new Date(task2.startdate).getTime() -
+                new Date(task1.startdate).getTime()
+            );
+        });
     }
 }
