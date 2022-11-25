50cea853fb2aef96c54ec3ea18e904779c0fa818
szmulikowski Vincent <89385456+Vincent1307@users.noreply.github.com>
2022-11-25 10:24:58 +0100

    modif index

---
+++ packages/frontend/src/app/index/index.component.ts.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { catchError, tap } from 'rxjs';
+import { HeaderTitleService } from '../services/header-title.service';
+
+@Component({
+    selector: 'app-index',
+    templateUrl: './index.component.html',
+    styleUrls: ['./index.component.css']
+})
+export class IndexComponent implements OnInit {
+    constructor(
+        private headerTitleService: HeaderTitleService,
+        private translateService: TranslateService
+    ) {
+        // translateService.use('en');
+        // translateService.use('fr');
+    }
+
+    ngOnInit(): void {
+        this.headerTitleService.setTitle('InfraScrum');
+    }
+}
